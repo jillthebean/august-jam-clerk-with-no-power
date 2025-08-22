59ca9c38fba61c36c65819b4ff3c021bbe71a062
JillyBean <jillybean@posteo.net>
2025-08-22 18:46:06 +0200

    improve book dragging functionality

--- scenes/book/book.gd
+++ scenes/book/book.gd
@@ -1,1 +1,20 @@
 extends RigidBody2D
+
+var dragging = false
+var offset = Vector2.ZERO
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		offset = event.position - self.global_position;
+		
+		if ($CollisionShape2D.shape.get_rect().has_point(offset)):
+			# Start dragging if the click is on the sprite.
+			if not dragging and event.pressed:
+				dragging = true
+		# Stop dragging if the button is released.
+		if dragging and not event.pressed:
+			dragging = false
+
+	if event is InputEventMouseMotion and dragging:
+		# While dragging, move the sprite with the mouse.
+		self.move_and_collide(event.position - self.global_position - offset)
--- world.gd
+++ world.gd
@@ -5,17 +5,18 @@ extends Node
 var dragging = false
 var click_radius = 32 # Size of the sprite.
 
-func _input(event):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		print("Dragging detected: ", (event.position - book.global_position).length())
-		if (event.position - book.global_position).length() < click_radius:
-			# Start dragging if the click is on the sprite.
-			if not dragging and event.pressed:
-				dragging = true
-		# Stop dragging if the button is released.
-		if dragging and not event.pressed:
-			dragging = false
-
-	if event is InputEventMouseMotion and dragging:
-		# While dragging, move the sprite with the mouse.
-		book.move_and_collide(event.position - book.global_position)
+#func _input(event):
+	#if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		#print("Dragging detected: ", book)
+		#
+		#if (event.position - book.global_position).length() < click_radius:
+			## Start dragging if the click is on the sprite.
+			#if not dragging and event.pressed:
+				#dragging = true
+		## Stop dragging if the button is released.
+		#if dragging and not event.pressed:
+			#dragging = false
+#
+	#if event is InputEventMouseMotion and dragging:
+		## While dragging, move the sprite with the mouse.
+		#book.move_and_collide(event.position - book.global_position)
